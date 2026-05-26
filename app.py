0cefe0c79ec21a37c1dac8d59fc811ced4ca7199
shivrajtadalgi <tadalgis@gmail.com>
2026-05-26 13:41:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ with col2:
 
     st.markdown(
         """
-        # 🚀 QPredict Enterprise Incident Intelligence Platform
+        # QPredict Enterprise Incident Intelligence Platform
 
         ### AI-Powered IT Operations Incident Severity Analytics
         """
